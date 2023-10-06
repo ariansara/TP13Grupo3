0defeb032a8fe005e29f6a601c543740a6376ba9
esrsosa <131368173+esrsosa@users.noreply.github.com>
2023-10-06 17:35:18 -0300

    a

--- src/tp13g3/TP13G3.java
+++ src/tp13g3/TP13G3.java
@@ -20,51 +20,55 @@ public class TP13G3 {
     /**
      * @param args the command line arguments
      */
-   public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException {
         try {
-            
+
             //Cargo Driver de conexión
             Class.forName("org.mariadb.jdbc.Driver");
-           
+
             //Establecer la conexión
-            String URL="jdbc:mariadb://localhost:3306/univesidadg3";
-            String usuario="root";
-            String password="";
-            Connection con=DriverManager.getConnection(URL,usuario,password);
+            String URL = "jdbc:mariadb://localhost:3306/univesidadg3";
+            String usuario = "root";
+            String password = "";
+            Connection con = DriverManager.getConnection(URL, usuario, password);
 //-----------------------------------------------------------------------------------------------------            
-            
-            String sql = "INSERT INTO alumno (dni, apellido, nombre, fechaDeNacimiento, estado)"
-                    
-                    + "VALUES (12344, 'Lopez', 'Juan', '2000-01-20', true),"
-                    + "       (35488, 'Sosa', 'Maria', '2003-01-18', true),"
-                      + "       (85655, 'Ponce', 'Roberto', '1999-08-7', true)";
-            
-            PreparedStatement ps=con.prepareStatement(sql);
-           int filas= ps.executeUpdate();
-            if(filas>0){
-            
+
+//            String sql = "INSERT INTO alumno (dni, apellido, nombre, fechaDeNacimiento, estado)"
+//                    
+//                    + "VALUES (12344, 'Lopez', 'Juan', '2000-01-20', true),"
+//                    + "       (35488, 'Sosa', 'Maria', '2003-01-18', true),"
+//                      + "       (85655, 'Ponce', 'Roberto', '1999-08-7', true)";
+//               String sql = "INSERT INTO materia (idmateria,nombre,anio,estado)"
+//                         +"VALUES (4,'naturales',2,true)";
+//                       +"         (1,'Lengua',1,true),"
+//                       +"         (2,'matematicas',3,true),"
+//                       +"         (3,'ingles',2,true)";
+//                       
+                String sql = "INSERT INTO inscripcion(idInscripto, nota, idAlumno, idMateria)"
+                        +  "VALUES (1,9,14,4),"
+                        +  "       (2,8,15,1),"
+                       +  "       (3,7,16,3)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            int filas = ps.executeUpdate();
+            if (filas > 0) {
                 JOptionPane.showMessageDialog(null, "Alumno Agregado");
             }
         } catch (ClassNotFoundException ex) {
-            JOptionPane.showMessageDialog(null,"Error al cargar Driver");
+            JOptionPane.showMessageDialog(null, "Error al cargar Driver");
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error de conexión");
-        
-        int error =ex.getErrorCode();
-        
-            if(error==1146){
-                JOptionPane.showMessageDialog(null,"Tabla inexistente");
-            }else if(error==1062){
-            
-                JOptionPane.showMessageDialog(null,"Dni duplicado");
-            }else if(error==1049){
-            
-                JOptionPane.showMessageDialog(null,"BD inexistente");
-            }else {
-            
-                JOptionPane.showMessageDialog(null,"Error SQL");
-                
+
+            int error = ex.getErrorCode();
+
+            if (error == 1146) {
+                JOptionPane.showMessageDialog(null, "Tabla inexistente");
+            } else if (error == 1062) {
+                JOptionPane.showMessageDialog(null, "Dni duplicado");
+            } else if (error == 1049) {
+                JOptionPane.showMessageDialog(null, "BD inexistente");
+            } else {
+                JOptionPane.showMessageDialog(null, "Error SQL");
             }
+        }
     }
-}
 }
